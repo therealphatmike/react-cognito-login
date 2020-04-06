6abd11323c833b2dfa83302a3c875f29616967ab
T Michael Smith <timothy.smith2@asurion.com>
2020-04-05 20:19:03 -0500

    built signup flow

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
 
       { /* catch all unmatched routes */ }
       <Route component={NotFound} />
